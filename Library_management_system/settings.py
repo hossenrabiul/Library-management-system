c431b85309383d17aded86315e7d10328f520cc8
hossenrabiul <robiulhossen0081@gmail.com>
2025-01-24 10:27:09 +0600

    code changed

--- Library_management_system/settings.py
+++ Library_management_system/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = [' https://library-management-system-mtbh.onrender.com']
-# CSRF_TRUSTED_ORIGINS = ['https://library-management-system-mtbh.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://library-management-system-mtbh.onrender.com','https://*.127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
